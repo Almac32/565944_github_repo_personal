d6478fbb9d54ac0cec22055dccc4eab04af688bc
Alejo Mac Donal <alejomacdonal@hotmail.com>
2026-04-01 19:08:17 -0300

    caca

--- Repaso.py
+++ Repaso.py
@@ -2,17 +2,20 @@ print("Calculadora en Python")
 
  
 numero1 = int(input("Ingrese el primer número: ")) 
-
 numero2 = int(input("Ingrese el segundo número: ")) 
  
-
 # Sumar los números y multiplicar
 
 suma = numero1 + numero2 
 multiplicacion= numero1*numero2
 
-# Mostrar el resultado 
+# Mostrar el resultado de la suma y resta
+
+
+
+
+
+
 
 print(f"El resultado de la suma es: {suma}")
 print("el resultado de la multipricacion es", multiplicacion) 
-
