3409a3704dabc2a78131a27a9c4a046cadad78f7
Alejo Mac Donal <alejomacdonal@hotmail.com>
2026-04-01 19:05:29 -0300

    mi primer commit

---
+++ Repaso.py
@@ -0,0 +1,18 @@
+print("Calculadora en Python") 
+
+ 
+numero1 = int(input("Ingrese el primer número: ")) 
+
+numero2 = int(input("Ingrese el segundo número: ")) 
+ 
+
+# Sumar los números y multiplicar
+
+suma = numero1 + numero2 
+multiplicacion= numero1*numero2
+
+# Mostrar el resultado 
+
+print(f"El resultado de la suma es: {suma}")
+print("el resultado de la multipricacion es", multiplicacion) 
+
